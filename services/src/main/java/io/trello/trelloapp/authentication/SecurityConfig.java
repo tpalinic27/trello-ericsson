adeb24d8ae4064f19ba1809a9ab23a10d0a9fc5c
Tomislava Palinić <tpalinic27@gmail.com>
2021-01-04 15:38:38 +0100

    Update SecurityConfig.java

--- services/src/main/java/io/trello/trelloapp/authentication/SecurityConfig.java
+++ services/src/main/java/io/trello/trelloapp/authentication/SecurityConfig.java
@@ -1,35 +1,16 @@
 package io.trello.trelloapp.authentication;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-//mport org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private UserService userDetailsService;
-
-    @Autowired
-    public JwtFilter jwtFilter;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
-    // @Bean
-    // public PasswordEncoder passwordEncoder(){
-    //     return NoOpPasswordEncoder.getInstance();
-    // }
 
     @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
     @Override
