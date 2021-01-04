227c650fcef603804e8d3ff810f7efca0eb17e01
Tomislava Palinić <tpalinic27@gmail.com>
2021-01-04 15:35:48 +0100

    Delete UserRepository.java

--- services/src/main/java/io/trello/trelloapp/authentication/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package io.trello.trelloapp.authentication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserName(String username);
-}
